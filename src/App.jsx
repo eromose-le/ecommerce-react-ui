2feac88dd6190e58033aff0bc89c0a3f102e14ab
eromose-le <erons24@yahoo.com>
2021-10-02 01:48:46 +0100

    cart-page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Login from './pages/Login';
+import Cart from './pages/Cart';
+// import Login from './pages/Login';
 // import Register from './pages/Register';
 // import Product from './pages/Product';
 // import ProductList from './pages/ProductList';
 // import Home from './pages/Home';
 
 const App = () => {
-  return <Login />;
+  return <Cart />;
 };
 
 export default App;
